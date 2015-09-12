0ffe43d7346277cb345215a3bec06229945f38d1
Brandon Him <bhim@anthropologie.com>
2015-09-12 04:13:15 -0400

    Update config and changes

--- mobile-config.js
+++ mobile-config.js
@@ -11,23 +11,23 @@ App.info({
 // Set up resources such as icons and launch screens.
 App.icons({
   // Android
-  'android_ldpi': 'resources/icons/icon-36x36.png',
-  'android_mdpi': 'resources/icons/icon-48x48.png',
-  'android_hdpi': 'resources/icons/icon-72x72.png',
-  'android_xhdpi': 'resources/icons/icon-96x96.png'
+  'android_ldpi': 'resources/icons/Icon-Small.png',
+  'android_mdpi': 'resources/icons/Icon-Small-50.png',
+  'android_hdpi': 'resources/icons/Icon-76@2x.png',
+  'android_xhdpi': 'resources/icons/Icon@2x.png'
   // ... more screen sizes and platforms ...
 });
 
 App.launchScreens({
-  // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+  // // Android
+  // 'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  // 'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  // 'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  // 'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  // 'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  // 'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  // 'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  // 'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
