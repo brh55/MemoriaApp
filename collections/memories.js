6dad9fbc0f537ecfe4d1f442fed69b7e29441e01
ala25 <ala25@njit.edu>
2015-09-12 10:34:58 -0400

    having issues sumbiting form

--- collections/memories.js
+++ collections/memories.js
@@ -1,1 +1,38 @@
 Memories = new Mongo.Collection('memories');
+if (Meteor.isClient) {
+    // This code only runs on the client
+    Template.create.helpers({
+        memories: function () {
+            return Memories.find({});
+        }
+    });
+    Template.create.events({
+        "submit .newMemory": function (event) {
+            console.log(event);
+            console.log("Form submitted");
+            console.log(event.target.text);
+
+            function delay(ms) {
+                ms += new Date().getTime();
+                while (new Date() < ms) {}
+            }
+            delay(5000);
+            //      // Prevent default browser form submit
+            //      event.preventDefault();
+            //        
+            // 
+            //      // Get value from form element
+            //      var text = event.target.text;
+            //        console.log(text);
+            // 
+            //      // Insert a task into the collection
+            //      Memories.insert({
+            //        text: text,
+            //        createdAt: new Date() // current time
+            //      });
+            // 
+            //      // Clear form
+            //      event.target.text.value = "";
+        }
+    });
+}
