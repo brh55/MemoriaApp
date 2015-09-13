e9f76f467c5ed36f0fdbc09eede19e22a02f0ee0
Brandon Him <bhim@anthropologie.com>
2015-09-13 19:27:51 -0400

    Clean up final directory for better repo presentation

--- client/templates/js/memory.js
+++ client/templates/js/memory.js
@@ -18,22 +18,4 @@ Template.memory.helpers({
 
         return memoriesArray;
     }
-    // coverPhoto: function() {
-    //     var cover = Memories.find().fetch();
-    //     console.log(Meteor.userId());
-    //     // Temporarily
-    //     return 'https://unsplash.com/photos/uXzNVOZT5no/download';
-    // },
-    // title: function() {
-    //     return 'A Tempest Night.';
-    // },
-    // location: function() {
-    //     return 'Hawaii';
-    // },
-    // date: function() {
-    //     return '08.20.15';
-    // },
-    // description: function() {
-    //     return 'Mauris malesuada blandit augue. Nam volutpat sapien sed dolor facilisis pharetra. Quisque posuere augue eu fringilla accumsan. Donec ultricies sem vitae nibh lacinia venenatis. Donec placerat ornare ultricies.'
-    // }
 });
